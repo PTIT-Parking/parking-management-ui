26434b7f7e49349ab0fd7b8239b9811beb3414ed
Lezh1n <hoangphilong1208@gmail.com>
2025-04-13 16:59:25 +0700

    config for deployment

--- next.config.js
+++ next.config.js
@@ -3,13 +3,9 @@ const nextConfig = {
   /* config options here */
   devIndicators: false,
   output: 'standalone',
-  poweredByHeader: false, // Loại bỏ header "Powered by Next.js"
+  poweredByHeader: false,
   compiler: {
-    removeConsole: process.env.NODE_ENV === 'production', // Xóa console.log trong production
-  },
-  // Để tối ưu hóa các hình ảnh từ domains cụ thể
-  images: {
-    domains: ['your-image-domain.com'],
+    removeConsole: process.env.NODE_ENV === 'production', 
   },
 };
 
